2df37e3b9fdeedb0c61976f41fc45fe188ff7656
AminMezouer <amezouer@gmail.com>
2025-10-15 16:46:14 +0200

    V4

--- src/lib.rs
+++ src/lib.rs
@@ -19,33 +19,73 @@ impl Tree {
         Tree(None)
     }
 
-    /// Returns a tree containing a single value
-    fn leaf(v: i32) -> Self {
-        Tree(Some(Box::new(Node {
-        value: v,
-        left: Tree::new(),
-        right: Tree::new(),
-        })))
-    }
-
-    /// Inserts `value` into the tree.
-    /// Returns `false` iff the `value` was already contained in the tree.
-    pub fn insert(&mut self, value: i32) -> bool {
-        if Tree
-        panic!("Not implemented")
+    pub fn insert(&mut self, val1: i32) -> bool {
+        if self.value == val1 {
+            false
+        } else if val1 < self.value {
+            match &mut self.left {
+                Some(left) => left.insert(val1),
+                None => {
+                    self.left = Some(Box::new(Node::leaf(val1)));
+                    true
+                }
+            }
+        } else { 
+            match &mut self.right {
+                Some(right) => right.insert(val1),
+                None => {
+                    self.right = Some(Box::new(Node::leaf(val1)));
+                    true
+                }
+            }
+        }
     }
 
     /// Returns true if and only if `value` belongs to the tree.
-    pub fn contains(&self, value: i32) -> bool {
-        panic!("Not implemented")
+    pub fn contains(&self, val2: i32) -> bool {
+        if self.value == val2 {
+            true
+        } else {
+            if self.value > val2 {
+                match &self.left {
+                    None => false,
+                    Some(left) => left.contains(val2),
+                }
+            } else {
+                match &self.right {
+                    None => false,
+                    Some(right) => right.contains(val2),
+                }
+            }
+        }
     }
 
     /// Deletes `value` from the tree.
     /// When the value is not found the tree, `false` is returned.
-    pub fn delete(&mut self, value: i32) {
-        panic!("Not implemented");
+    pub fn delete(&mut self, val: i32) {
+    if self.value == val {
+        self.value = 0;
+        self.left = None;
+        self.right = None;
+    } else if val < self.value {
+        match &mut self.left {
+            Some(left) => {
+                left.delete(val);
+            }
+            None => {}
+        }
+    } else {
+        match &mut self.right {
+            Some(right) => {
+                right.delete(val);
+            }
+            None => {}
+            }
+        }
     }
+
 }
+
 
 fn main() {
     let t = Tree::new();
